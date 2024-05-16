daac6ac4bdbbf077b02d240df41b5bda424c3377
Fatime01 <memiyevafatime280@gmail.com>
2024-05-16 10:52:45 +0400

    1 filed changed

--- src/script/script.js
+++ src/script/script.js
@@ -1,8 +1,51 @@
 const first = (vol) => {
-let x = 9;
-vol 
-for(let i = 0; i < vol.length; i++)
-x = Math.sqrt(x);
-console.log(x);
+    let kök = Math.sqrt(vol)
+    console.log(kök);
 }
-first(Math.sqrt)
+
+first(16)
+
+
+const whichLetter = (str) => {
+    let sait = ''
+    let samit = ''
+    let lower = str.toLowerCase()
+    
+    for (let i of lower) {
+      switch (i) {
+        case 'a' :
+        case 'e' :
+        case 'ə' :
+        case 'i' :
+        case 'ı' :
+        case 'o' :
+        case 'ö' :
+        case 'u' :
+        case 'ü' :
+          sait += i
+          break;
+        case ' ' :
+          continue;
+        default :
+        samit += i
+      }
+    }
+  
+    console.log(`saitlər - ${sait}`);
+    console.log(`saitlərin sayı - ${sait.length}`);
+    console.log(`samitlər - ${samit}`);
+    console.log(`samitlərin sayı - ${samit.length}`);
+  }
+  whichLetter('hORiZon loREm')
+
+
+const combine = (arr) => {
+  let result = ''
+
+  for (let i of arr) {
+    let str = i.toString()
+    result += str
+  }
+  console.log(`cavab - ${result}`);
+}
+combine([8, 'name', 30, 'lorem'])
